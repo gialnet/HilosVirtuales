d947f57a9f2cde8f9d28902c21bf2e2abe1899d7
gialnet <antonio.gialnet@gmail.com>
2026-06-05 12:52:07 +0200

    Init commit

---
+++ src/main/java/com/vivaldi_spring/HilosVirtuales/HilosVirtualesApplication.java
@@ -0,0 +1,13 @@
+package com.vivaldi_spring.HilosVirtuales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilosVirtualesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HilosVirtualesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivaldi_spring/HilosVirtuales/HilosVirtualesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivaldi_spring.HilosVirtuales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilosVirtualesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
